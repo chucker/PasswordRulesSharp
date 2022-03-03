aa83421d9274f199f60dd23afc3cd8a23cdaa286
Sören Kuklau <kuklau@trenz.de>
2022-03-03 16:11:19 +0100

    began parser

---
+++ PasswordRulesSharp.Tests/Parser/IsValidTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace PasswordRulesSharp.Tests.Parser
+{
+    public class IsValidTests
+    {
+        [TestCase("minlength: 20; required: lower; required: upper; required: digit; required: [-];")]
+        public void IsValid(string rule)
+        {
+            var parser = new PasswordRulesSharp.Parser();
+            Assert.True(parser.IsValid(rule));
+        }
+
+        [TestCase("asd")]
+        public void IsInvalid(string rule)
+        {
+            var parser = new PasswordRulesSharp.Parser();
+            Assert.False(parser.IsValid(rule));
+        }
+    }
+}
---
+++ PasswordRulesSharp/Parser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PasswordRulesSharp
+{
+    public class Parser
+    {
+        const string RegexPattern = @"(?<Name>[\w-]+):\s*(?<Value>[^;]+);";
+
+        public bool IsValid(string rule)
+        {
+            var match = System.Text.RegularExpressions.Regex.Match(rule, RegexPattern);
+
+            return true;
+        }
+    }
+}
