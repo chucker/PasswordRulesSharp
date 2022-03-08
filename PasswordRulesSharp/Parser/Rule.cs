0a08c295b4bf3f1dde8a52bb13f0015a0c86c4c9
Sören Kuklau <kuklau@trenz.de>
2022-03-08 14:24:13 +0100

    Some XML docs

--- PasswordRulesSharp/Parser/Rule.cs
+++ PasswordRulesSharp/Parser/Rule.cs
@@ -11,10 +11,36 @@ namespace PasswordRulesSharp.Parser
             MaximumLength
         }
 
+        /// <summary>
+        /// The minimum length of a valid password, in chars.
+        /// </summary>
         public int? MinLength { get; }
+
+        /// <summary>
+        /// <para>
+        /// The maximum length of a valid password, in chars.
+        /// </para>
+        /// 
+        /// <para>
+        /// If <see cref="MinLength"/> is also set, must be greater or equal.
+        /// </para>
+        /// </summary>
         public int? MaxLength { get; }
 
-        public CharacterClass? Required { get; }
+        /// <summary>
+        /// <para>
+        /// A list of required character classes.
+        /// </para>
+        /// 
+        /// <para>
+        /// Each of these is at-least-one-of. For example, <c>required: lower;
+        /// required: upper;</c> means at least one lower-case _and_ at least
+        /// one upper-case character is required. <c>required: [!@]</c> means
+        /// _one_ of those two special chars is required, whereas
+        /// <c>required: [!]; required: [@];</c> means _both_ are required.
+        /// </para>
+        /// </summary>
+        public List<CharacterClass>? Required { get; }
 
         public Rule(string rule)
         {
