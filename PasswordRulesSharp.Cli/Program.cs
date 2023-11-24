de9d2afd29a1af09e239ab989dfd04fbf56f8eba
Sören Kuklau <kuklau@trenz.de>
2023-11-24 12:00:41 +0100

    a CLI, currently for generating passwords based on a rule

---
+++ PasswordRulesSharp.Cli/Program.cs
@@ -0,0 +1,19 @@
+﻿using PasswordRulesSharp.Cli.Commands;
+
+using Spectre.Console.Cli;
+
+var app = new CommandApp();
+
+app.Configure(config =>
+{
+    config.AddCommand<GenerateCommand>("generate")
+        .WithAlias("gen")
+        .WithDescription("Generates a bunch of passwords based on a rule.");
+
+#if DEBUG
+    config.PropagateExceptions();
+    config.ValidateExamples();
+#endif
+});
+
+app.Run(args);
