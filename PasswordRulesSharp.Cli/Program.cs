90c2ce5b14b20ef2455ba6c10f3834e4033a81aa
Sören Kuklau <>
2024-11-20 21:01:14 +0100

    Cli: add two `generate` invocation examples

--- PasswordRulesSharp.Cli/Program.cs
+++ PasswordRulesSharp.Cli/Program.cs
@@ -8,7 +8,9 @@ app.Configure(config =>
 {
     config.AddCommand<GenerateCommand>("generate")
         .WithAlias("gen")
-        .WithDescription("Generates a bunch of passwords based on a rule.");
+        .WithDescription("Generates a bunch of passwords based on a rule.")
+        .WithExample("generate", "--rule=\"minlength: 32\"")
+        .WithExample("generate", "--rule=\"minlength: 20; required: lower; required: upper; required: digit; required: [-];\"");
 
 #if DEBUG
     config.PropagateExceptions();
